02af61654616c0325a7a0baf94f41fd913ca71e0
thalialongaray <56452075+thalialongaray@users.noreply.github.com>
2021-09-16 18:53:18 -0300

    Update TDM.java

--- ProcessadorMacro - GUI/src/main/java/ps/processadormacro/TDM.java
+++ ProcessadorMacro - GUI/src/main/java/ps/processadormacro/TDM.java
@@ -3,91 +3,161 @@ import java.util.ArrayList;
 import static ps.processadormacro.PM.linhas;
 
 public class TDM {
-    private ArrayList<String> prototipos, definicoes, parametros;
-    private ArrayList<Integer> contador;
+    private ArrayList<String> prototipos, definicoes, parametros, prototiposMacrosInternas, parametrosMacrosInternas;
+    private ArrayList<Integer> contador, numeroExpansoes;
    
     public TDM() {
         this.contador = new ArrayList<>();
         this.definicoes = new ArrayList<>();
         this.parametros = new ArrayList<>();
         this.prototipos = new ArrayList<>();
+        this.numeroExpansoes = new ArrayList<>();
+        this.prototiposMacrosInternas = new ArrayList<>();
+        this.parametrosMacrosInternas = new ArrayList<>();
     }
     
-    protected void setPrototipoMacro(String prototipo, int aux) {
-        prototipos.add(prototipo.substring(0, aux).concat(prototipo.substring(aux + 6)));
+    protected void setPrototipoMacro(String prototipo) {
+        prototipos.add(prototipo.substring(0, prototipo.indexOf("MACRO")).concat(prototipo.substring(prototipo.indexOf("MACRO") + 6)));
         contador.add(definicoes.size());
-        setParametros(prototipo.substring(aux + 6));
+        numeroExpansoes.add(0);
+        setParametros(prototipo.substring(prototipo.indexOf("MACRO") + 6));
+    }
+    
+    private void setParametros(String particaoLinha) {
+        String parametrosLinha[];
+        int aux = particaoLinha.indexOf(";"), cont;
+        if(aux != -1) {
+            particaoLinha = particaoLinha.substring(0, aux);
+        }
+        parametrosLinha = particaoLinha.split(",");
+        
+        for(cont = 0; cont < parametrosLinha.length; cont++) {
+            if(parametrosLinha[cont].length() > 1 && parametrosLinha[cont].contains(" ")) {
+                parametrosLinha[cont] = parametrosLinha[cont].trim();
+            }
+            parametros.add(parametrosLinha[cont]);
+        }
+    }
+    
+    protected void setDefinicaoMacro(String linha, boolean macroInterna) {
+        String temp = linha;
+        if(temp.contains(";")) {
+            temp = temp.substring(0, temp.indexOf(";"));
+        }
+
+        int cont;
+        if(macroInterna) {
+            if(temp.contains("ENDM")) {                                         //finaliza macro interna
+                definicoes.add(temp);
+
+                temp = prototiposMacrosInternas.get(prototiposMacrosInternas.size() - 1);
+                String parametrosMacroInterna[];
+                parametrosMacroInterna = temp.substring(temp.indexOf(" ")).split(",");
+                for(cont = 0; cont < parametrosMacroInterna.length; cont++) {
+                    parametrosMacrosInternas.remove(parametrosMacrosInternas.size() - 1);
+                }
+                prototiposMacrosInternas.remove(prototiposMacrosInternas.size() - 1);
+            } else if(temp.contains("MACRO")) {                                 //abre macro interna
+                definicoes.add(temp);
+                
+                prototiposMacrosInternas.add(temp.substring(0, temp.indexOf("MACRO")).concat(temp.substring(temp.indexOf("MACRO") + 6)));
+                temp = temp.substring(temp.indexOf("MACRO") + 6);
+                String parametrosMacroInterna[] = temp.split(",");
+                parametrosMacroInterna = limpaEspacosParametros(parametrosMacroInterna);
+                for(cont = 0; cont < parametrosMacroInterna.length; cont++) {
+                    parametrosMacrosInternas.add(parametrosMacroInterna[cont]);
+                }
+            } else {                                                            //adiciona linha macro interna
+                String parametrosLinha[];
+                int contParametrosFormais, contParametrosMacrosInternas;
+                boolean substituir;
+
+                if(!temp.trim().contains(" ")) {                                //string única
+                    for(contParametrosFormais = 0; contParametrosFormais < parametros.size(); contParametrosFormais++) {
+                        substituir = true;
+                        for(contParametrosMacrosInternas = 0; contParametrosMacrosInternas < parametrosMacrosInternas.size(); contParametrosMacrosInternas++) {
+                            if(parametrosMacrosInternas.get(contParametrosMacrosInternas).contentEquals(temp.trim())) {
+                                substituir = false;
+                            }
+                        }
+                        if(substituir && parametros.get(contParametrosFormais).contentEquals(temp.trim())) {
+                            int j = indexOcorrenciaParametro(temp, temp.trim());
+                            temp = temp.substring(0, temp.indexOf(parametros.get(contParametrosFormais), j)).concat("#" + contParametrosFormais + 
+                                    temp.substring(temp.indexOf(parametros.get(contParametrosFormais), j) + temp.trim().length()));
+                        }
+                    }
+                } else {
+                    parametrosLinha = temp.replace(",", " ").split(" ");
+                    parametrosLinha = limpaEspacosParametros(parametrosLinha);
+                    
+                    for(contParametrosFormais = 0; contParametrosFormais < parametros.size(); contParametrosFormais++) {
+                        substituir = true;
+                        for(cont = 0; cont < parametrosLinha.length; cont++) {
+                            for(contParametrosMacrosInternas = 0; contParametrosMacrosInternas < parametrosMacrosInternas.size(); contParametrosMacrosInternas++) {
+                                if(parametrosMacrosInternas.get(contParametrosMacrosInternas).contentEquals(parametrosLinha[cont])) {
+                                    substituir = false;
+                                }
+                            }
+                            if(substituir && parametros.get(contParametrosFormais).contentEquals(parametrosLinha[cont])) {
+                                int j = indexOcorrenciaParametro(temp, parametrosLinha[cont]);
+                                temp = temp.substring(0, temp.indexOf(parametros.get(contParametrosFormais), j)).concat("#" + contParametrosFormais + 
+                                        temp.substring(temp.indexOf(parametros.get(contParametrosFormais), j) + parametrosLinha[cont].length()));
+                            }
+                        }
+                    }
+                }
+                definicoes.add(temp);
+            }
+        } else {
+            if(!temp.contains("ENDM")) {
+                String parametrosLinha[];
+                int contParametrosFormais;
+
+                if(!temp.trim().contains(" ")) {                                //string única
+                    for(contParametrosFormais = 0; contParametrosFormais < parametros.size(); contParametrosFormais++) {
+                        if(parametros.get(contParametrosFormais).contentEquals(temp.trim())) {
+                            int j = indexOcorrenciaParametro(temp, temp.trim());
+                            temp = temp.substring(0, temp.indexOf(parametros.get(contParametrosFormais), j)).concat("#" + contParametrosFormais + 
+                                    temp.substring(temp.indexOf(parametros.get(contParametrosFormais), j) + temp.trim().length()));
+                        }
+                    }
+                } else {
+                    parametrosLinha = temp.replace(",", " ").split(" ");
+                    parametrosLinha = limpaEspacosParametros(parametrosLinha);
+
+                    for(contParametrosFormais = 0; contParametrosFormais < parametros.size(); contParametrosFormais++) {
+                        for(cont = 0; cont < parametrosLinha.length; cont++) {
+                            if(parametros.get(contParametrosFormais).contentEquals(parametrosLinha[cont])) {
+                                int j = indexOcorrenciaParametro(temp, parametrosLinha[cont]);
+                                temp = temp.substring(0, temp.indexOf(parametros.get(contParametrosFormais), j)).concat("#" + contParametrosFormais + 
+                                        temp.substring(temp.indexOf(parametros.get(contParametrosFormais), j) + parametrosLinha[cont].length()));
+                            }
+                        }
+                    }
+                }
+            }
+            definicoes.add(temp);
+        }
     }
 
-    protected void setParametros(String prototipo) {
-        int aux = prototipo.indexOf(";"), auxCorte, cont;
-        if(aux != -1) {
-            prototipo = prototipo.substring(0, aux);
-        } else {
-            aux = prototipo.length();
-        }
-        
-        for(cont = 0; cont < aux; cont++) {
-            if(prototipo.contains(",")) {
-                parametros.add(prototipo.substring(0, prototipo.indexOf(",")));
-                auxCorte = prototipo.indexOf(",");
-                cont += auxCorte - 1;
-                prototipo = prototipo.substring(auxCorte);
-                while(prototipo.indexOf(",") == 0 || prototipo.indexOf(" ") == 0) {
-                    cont++;
-                    prototipo = prototipo.substring(1);
-                }
-            } else if(prototipo.contains(" ")) {
-                parametros.add(prototipo.substring(0, prototipo.indexOf(" ")));
-                auxCorte = prototipo.indexOf(" ");
-                cont += auxCorte - 1;
-                prototipo = prototipo.substring(auxCorte);
-                while(prototipo.indexOf(",") == 0 || prototipo.indexOf(" ") == 0) {
-                    cont++;
-                    prototipo = prototipo.substring(1);
-                }
-            } else if(prototipo.length() != 0) {
-                parametros.add(prototipo);
-                cont = aux;
+    protected int indexOcorrenciaParametro(String linha, String parametro) {
+        int i, j = 0;
+        String s = linha.replace(",", " ");
+        for(i = 0; i < s.split(" ").length; i++) {
+            if(s.split(" ")[i].contentEquals(parametro)) {
+                break;
             }
+            j += s.split(" ")[i].length() + 1;
         }
+        return j;
     }
     
-    protected void setDefinicaoMacro(int indexLinha) {
-        String temp = linhas.get(indexLinha).replace("\t", " ");
-        int cont, aux = 0;
-        for(cont = 0; cont < parametros.size(); cont++) {
-            if(temp.contains(parametros.get(cont))) {
-                aux = temp.indexOf(parametros.get(cont), aux);
-                if((aux > 0 && (temp.length() - aux > parametros.get(cont).length()) &&
-                        (temp.substring(aux - 1, aux + parametros.get(cont).length() + 1).contains(" " + parametros.get(cont) + " ") ||
-                        temp.substring(aux - 1, aux + parametros.get(cont).length() + 1).contains("," + parametros.get(cont) + " ") ||
-                        temp.substring(aux - 1, aux + parametros.get(cont).length() + 1).contains(" " + parametros.get(cont) + ",") ||
-                        temp.substring(aux - 1, aux + parametros.get(cont).length() + 1).contains("," + parametros.get(cont) + ","))) ||
-                        (aux == 0 && (temp.length() - aux > parametros.get(cont).length()) &&
-                        (temp.substring(aux, parametros.get(cont).length() + 1).contains(parametros.get(cont) + " ") ||
-                        temp.substring(aux, parametros.get(cont).length() + 1).contains(parametros.get(cont) + ",")))) {
-                    temp = temp.substring(0, aux).concat("#" + cont + temp.substring(aux + parametros.get(cont).length()));
-                    cont--;
-                } else if((aux > 0 && (temp.length() - aux == parametros.get(cont).length()) &&
-                        (temp.substring(aux - 1).contains(" " + parametros.get(cont)) ||
-                        temp.substring(aux - 1).contains("," + parametros.get(cont)))) ||
-                        (aux == 0 && (temp.length() - aux == parametros.get(cont).length()) &&
-                        temp.substring(aux).contains(parametros.get(cont)))) {
-                    temp = temp.substring(0, aux).concat("#" + cont);
-                } else if(aux >= 0) {
-                    aux += parametros.get(cont).length();
-                    cont--;
-                }
-                
-                
-            }
-        }
-        definicoes.add(temp);
+    protected void setNumeroExpansoes(int indexPrototipo, int numero) {
+        numeroExpansoes.set(indexPrototipo, numero);
     }
     
-    protected void limpaParametros() {
-        parametros.clear();
+    protected int getNumeroExpansoes(int indexPrototipo) {
+        return numeroExpansoes.get(indexPrototipo);
     }
     
     protected int getNumeroMacrosDefinidas() {
@@ -95,12 +165,12 @@ public class TDM {
     }
     
     protected String getNomeMacro(int index) {
-        int aux = prototipos.get(index).indexOf(" ") - 1;
-        return prototipos.get(index).substring(0, aux);
+        return prototipos.get(index).substring(0, prototipos.get(index).indexOf(" "));
     }
     
-    protected ArrayList<String> getParametrosChamada(int indexChamada) {
-        setParametros(linhas.get(indexChamada).substring(linhas.get(indexChamada).indexOf(" ") + 1));
+    protected ArrayList<String> getParametrosChamada(int indexPrototipo, int indexChamada) {
+        String nomeMacro = getNomeMacro(indexPrototipo), chamada = linhas.get(indexChamada);
+        setParametros(chamada.substring(chamada.indexOf(nomeMacro) + nomeMacro.length() + 1));
         return parametros;
     }
     
@@ -110,5 +180,19 @@ public class TDM {
     
     protected String getLinhaDefinicao(int indexLinha) {
         return definicoes.get(indexLinha);
+    }
+    
+    protected void limpaParametros() {
+        parametros.clear();
+    }
+    
+    protected String[] limpaEspacosParametros(String[] parametros) {
+        int cont;
+        for(cont = 0; cont < parametros.length; cont++) {
+            if(parametros[cont].length() > 1 && parametros[cont].contains(" ")) {
+                parametros[cont] = parametros[cont].trim();
+            }
+        }
+        return parametros;
     }
 }
